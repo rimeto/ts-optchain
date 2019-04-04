30bca7b8a40a53f3e95d92e65b3f324b6ca481d9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-04-05 02:29:05 +0300

    Clean up package build

--- __tests__/ts-optchain-test.ts
+++ __tests__/ts-optchain-test.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { oc } from '../index';
+import { oc } from '../src';
 
 describe('ts-optchain', () => {
   it('sanity checks', () => {
