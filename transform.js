7540b275d7040d789f4eb3e0d9c7a83940495dea
Neville Bowers <nsb@rimeto.io>
2019-04-05 12:11:47 -0700

    updates

---
+++ transform.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright (C) 2019-present, Rimeto, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+exports = require('dist/transform');
